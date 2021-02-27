ffe2abe787647eda2e69bab524d44bab6929dd2a
Sergio Di Fiore <difiores@outlook.com>
2021-02-27 18:36:54 -0300

    Add project files.

---
+++ BubbleSort/Classifica.cs
@@ -0,0 +1,38 @@
+﻿namespace BubbleSort
+{
+    public static class Classifica
+    {
+
+        public static void Ascende(int dimensao, double[] sortArray)
+        {
+            for (int i = dimensao - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (sortArray[j] > sortArray[j + 1])
+                    {
+                        var calice = sortArray[j];
+                        sortArray[j] = sortArray[j + 1];
+                        sortArray[j + 1] = calice;
+                    }
+                }
+            }
+        }
+
+        public static void Descende(int dimensao, double[] sortArray)
+        {
+            for (int i = dimensao - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (sortArray[j] < sortArray[j + 1])
+                    {
+                        var calice = sortArray[j];
+                        sortArray[j] = sortArray[j + 1];
+                        sortArray[j + 1] = calice;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Quantos dados deseja classificar?");
+            var ehNumero = int.TryParse(Console.ReadLine(), out int dimensao);
+
+            if (!ehNumero)
+                dimensao = 100;
+
+            string opcao = "0";
+
+            while (opcao != "1" && opcao != "2")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Escolha a opção de classificação:");
+                Console.WriteLine("[1] Ascendente    [2] Descendente");
+                opcao = Console.ReadKey().KeyChar.ToString();
+            }
+
+            Console.Clear();
+
+            double[] sortArray = new double[dimensao];
+
+            for (int i = 0; i < dimensao; i++)
+            {
+                Console.WriteLine($"Digite o valor do {i + 1}º elemento:");
+                sortArray[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("** Início do processamento - Aguarde **");
+
+            if (opcao == "1")
+                Classifica.Ascende(dimensao, sortArray);
+            else
+                Classifica.Descende(dimensao, sortArray);
+
+            foreach (var item in sortArray)
+                Console.WriteLine(item);
+        }
+    }
+}
